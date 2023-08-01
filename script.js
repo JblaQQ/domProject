e7f769f777b2ebe82a314db7c327f145193625fd
JULIA AHONSI <ahonsijulia@gmail.com>
2023-08-01 10:18:51 +0100

    css file edited and modified

---
+++ script.js
@@ -0,0 +1,6 @@
+const backgroundColor = document.querySelector(addcart)
+function changeBgColor () {
+    backgroundColor. style. backgroundColor = 'blue' 
+};
+backgroundColor.addEventListener ("mouseover");
+
