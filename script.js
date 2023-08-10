65bdcf215b6d42fedf4e3d4b624c39f253021ab7
JULIA AHONSI <ahonsijulia@gmail.com>
2023-08-10 14:12:58 +0100

    dom project

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const backgroundColor = document.querySelector(addcart)
-function changeBgColor () {
-    backgroundColor. style. backgroundColor = 'blue' 
-};
-backgroundColor.addEventListener ("mouseover");
+const plusButtons = document.querySelector('#plusBtn');
+const minusButtons = document.querySelector('#minusBtn');
+const quantities = document.querySelector('#quantity');
+const addToCartButtons = document.querySelector('#addcart');
 
+//add event listeners to plus buttons
+plusButtons.forEach((button, index) => {
+    button.addEventListener('click', () =>
+    if (currentQuantity > 0) 
+    { quantityDisplays[index].textContent = currentQuantity - 1; 
+    } 
+}); 
